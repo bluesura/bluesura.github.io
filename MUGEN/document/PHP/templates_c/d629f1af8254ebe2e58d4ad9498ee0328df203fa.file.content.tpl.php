b5eb96533bc3a77f71c897305d701a3aad536d1d
bluesura <you@example.com>
2017-05-14 08:59:41 +0900

    修正
    見えないimgタグを埋め込み

--- MUGEN/document/PHP/templates_c/d629f1af8254ebe2e58d4ad9498ee0328df203fa.file.content.tpl.php
+++ MUGEN/document/PHP/templates_c/d629f1af8254ebe2e58d4ad9498ee0328df203fa.file.content.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.12, created on 2016-12-21 10:45:46
+<?php /* Smarty version Smarty-3.1.12, created on 2017-05-13 23:57:41
          compiled from "F:\bluesura\Dropbox\Public\www\mugen.github.io\MUGEN\document\Template\content.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:2500257944e155e1c36-62291719%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'd629f1af8254ebe2e58d4ad9498ee0328df203fa' => 
     array (
       0 => 'F:\\bluesura\\Dropbox\\Public\\www\\mugen.github.io\\MUGEN\\document\\Template\\content.tpl',
-      1 => 1482313152,
+      1 => 1494719844,
       2 => 'file',
     ),
   ),
@@ -27,7 +27,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_57944e156a4df0_80037659')) {function content_57944e156a4df0_80037659($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_replace')) include 'C:\\libs\\php\\smarty\\plugins\\modifier.replace.php';
 ?><div id="main-inner"><article class="entry hentry js-entry-article date-first autopagerize_page_element chars-200 words-100 mode-hatena entry-odd">
-	<header class="entry-header"<?php  $_smarty_tpl->tpl_vars['array'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['array']->_loop = false;
+  <header class="entry-header"<?php  $_smarty_tpl->tpl_vars['array'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['array']->_loop = false;
  $_from = $_smarty_tpl->tpl_vars['content']->value['images']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['array']->key => $_smarty_tpl->tpl_vars['array']->value){
 $_smarty_tpl->tpl_vars['array']->_loop = true;
@@ -42,11 +42,14 @@ px; background-color:rgba(255,255,255,0.4);display: -webkit-flex;"<?php } ?>><?p
 "><meta itemprop="height" content="<?php echo $_smarty_tpl->tpl_vars['content']->value['images'][0]['height'];?>
 "></span><?php }?>
 
-	<div class="entry-title" itemprop="name"><div>
-	<h1 id="<?php echo $_smarty_tpl->tpl_vars['content']->value['state'];?>
+  <img src="https://bluesura.github.io/MUGEN/document/State/media/img/<?php echo $_smarty_tpl->tpl_vars['content']->value['images'][0]['src'];?>
+" alt="<?php echo $_smarty_tpl->tpl_vars['content']->value['images'][0]['alt'];?>
+" style="display:none;">
+  <div class="entry-title" itemprop="name"><div>
+  <h1 id="<?php echo $_smarty_tpl->tpl_vars['content']->value['state'];?>
 ">Type = <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['content']->value['state'], ENT_QUOTES, 'ISO-8859-1', true);?>
 </h1>
-	<?php if ($_smarty_tpl->tpl_vars['content']->value['page']['category']!=null){?><div class="category"><?php echo htmlspecialchars($_smarty_tpl->tpl_vars['content']->value['page']['category'][0], ENT_QUOTES, 'ISO-8859-1', true);?>
+  <?php if ($_smarty_tpl->tpl_vars['content']->value['page']['category']!=null){?><div class="category"><?php echo htmlspecialchars($_smarty_tpl->tpl_vars['content']->value['page']['category'][0], ENT_QUOTES, 'ISO-8859-1', true);?>
  > <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['content']->value['page']['category'][1], ENT_QUOTES, 'ISO-8859-1', true);?>
  | <?php if ($_smarty_tpl->tpl_vars['content']->value['page']['version']!=null){?>実装されたバージョン: <?php echo $_smarty_tpl->tpl_vars['content']->value['page']['version'];?>
 <?php }?> | <?php if ($_smarty_tpl->tpl_vars['content']->value['page']['target']!=null){?>対象: <?php echo $_smarty_tpl->tpl_vars['content']->value['page']['target'];?>
@@ -56,7 +59,7 @@ px; background-color:rgba(255,255,255,0.4);display: -webkit-flex;"<?php } ?>><?p
 
 </div></header>
 
-	<div class="entry-content">
+  <div class="entry-content">
 
 <?php echo $_smarty_tpl->getSubTemplate ("./content/Description.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
@@ -95,6 +98,6 @@ $_smarty_tpl->tpl_vars['contributor']->_loop = true;
 ?><?php echo $_smarty_tpl->tpl_vars['contributor']->value;?>
 氏<?php if ($_smarty_tpl->tpl_vars['contributor']->last!=true){?>, <?php }?><?php } ?></div><?php }?></div>
 
-	</div>
+  </div>
 </article></div>
 <?php }} ?>
--- MUGEN/document/Template/content.tpl
+++ MUGEN/document/Template/content.tpl
@@ -1,15 +1,16 @@
 <div id="main-inner"><article class="entry hentry js-entry-article date-first autopagerize_page_element chars-200 words-100 mode-hatena entry-odd">
-	<header class="entry-header"{foreach $content.images as $array} style="background: url(./media/img/{$array['src']}) no-repeat; background-size: cover;"{/foreach}><div{foreach $content.images as $array} style="height:{$array['height']}px; background-color:rgba(255,255,255,0.4);display: -webkit-flex;"{/foreach}>{if $content.images!=NULL}<span itemprop="image" itemscope itemtype="https://schema.org/ImageObject" style="display: none;"/><meta itemprop="url" content="https://bluesura.github.io/MUGEN/document/State/media/img/{$content.images[0].src}"><meta itemprop="width" content="{$content.images[0].width}"><meta itemprop="height" content="{$content.images[0].height}"></span>{/if}
+  <header class="entry-header"{foreach $content.images as $array} style="background: url(./media/img/{$array['src']}) no-repeat; background-size: cover;"{/foreach}><div{foreach $content.images as $array} style="height:{$array['height']}px; background-color:rgba(255,255,255,0.4);display: -webkit-flex;"{/foreach}>{if $content.images!=NULL}<span itemprop="image" itemscope itemtype="https://schema.org/ImageObject" style="display: none;"/><meta itemprop="url" content="https://bluesura.github.io/MUGEN/document/State/media/img/{$content.images[0].src}"><meta itemprop="width" content="{$content.images[0].width}"><meta itemprop="height" content="{$content.images[0].height}"></span>{/if}
 
-	<div class="entry-title" itemprop="name"><div>
-	<h1 id="{$content.state}">Type = {$content.state|escape}</h1>
-	{if $content.page.category != NULL}<div class="category">{$content.page.category[0]|escape} > {$content.page.category[1]|escape} | {if $content.page.version != NULL}実装されたバージョン: {$content.page.version}{/if} | {if $content.page.target != NULL}対象: {$content.page.target}{/if}</div>
+  <img src="https://bluesura.github.io/MUGEN/document/State/media/img/{$content.images[0].src}" alt="{$content.images[0].alt}" style="display:none;">
+  <div class="entry-title" itemprop="name"><div>
+  <h1 id="{$content.state}">Type = {$content.state|escape}</h1>
+  {if $content.page.category != NULL}<div class="category">{$content.page.category[0]|escape} > {$content.page.category[1]|escape} | {if $content.page.version != NULL}実装されたバージョン: {$content.page.version}{/if} | {if $content.page.target != NULL}対象: {$content.page.target}{/if}</div>
 {/if}
 </div></div>
 
 </div></header>
 
-	<div class="entry-content">
+  <div class="entry-content">
 
 {include file="./content/Description.tpl"}
 
@@ -29,5 +30,5 @@
 
 <div><div><strong>公開日:</strong><time class="published" itemprop="datePublished" datetime="2008-06-05">2008.06.05</time></div>{if $content.page.update != NULL}<div><strong>最終更新日:</strong><time class="updated" itemprop="dateModified" datetime="{$content.page.update|replace:'.':'-'}">{$content.page.update}</time></div>{/if}{if $content.page.contributor != NULL}<div><strong>貢献者:</strong>{foreach $content.page.contributor as $contributor}{$contributor}氏{if $contributor@last != true}, {/if}{/foreach}</div>{/if}</div>
 
-	</div>
+  </div>
 </article></div>
